7803c860e9b18834e8b0f2447f10514017ab843f
Pedro Jean <pedrojean877@gmail.com>
2026-01-11 12:57:21 -0300

    tres

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_ADMIN_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_ADMIN_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("⚠️ ERRO CRÍTICO: Variáveis de ambiente do Supabase Admin não encontradas.");
+  throw new Error("⚠️ ERRO CRÍTICO: Variáveis de ambiente do Supabase Admin não encontradas no .env.local");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
